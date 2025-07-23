934784868de3021920c7e02577c155c1c6107743
bit-soul <marsOwnershipdao@aliyun.com>
2025-07-23 16:36:22 +0800

    refine support.js

--- test/support.js
+++ test/support.js
@@ -7,7 +7,11 @@ function randomInt() {
   return (Math.random() * 10000).toFixed(0);
 }
 
-function findRouterHandler(router, method, path) {
+function mockUser(user) {
+  return 'mock_user=' + JSON.stringify(user) + ';';
+}
+
+exports.findRouterHandler = function(router, method, path) {
   const route = router.stack.find(r => r.path === path && r.methods.includes(method));
   if (!route || router.stack.length !== 1) {
     return null;
@@ -15,43 +19,39 @@ function findRouterHandler(router, method, path) {
   return route.stack[0].handle;
 }
 
-const createUser = exports.createUser = async function () {
+exports.createUser = async function() {
   const key = new Date().getTime() + '_' + randomInt();
   const passhash = await tools.bhash('pass');
   return await User.newAndSaveUser('alsotang' + key, passhash, 'alsotang' + key + '@gmail.com', false);
 };
 
-exports.createUserByNameAndPwd = async function (loginname, pwd) {
+exports.createUserByNameAndPwd = async function(loginname, pwd) {
   const passhash = await tools.bhash(pwd);
   return await User.newAndSaveUser(loginname, passhash, loginname + +new Date() + '@gmail.com', true);
 };
 
-const createTopic = exports.createTopic = async function (authorId) {
+exports.createTopic = async function(authorId) {
   const key = new Date().getTime() + '_' + randomInt();
   return await Topic.newAndSaveTopic('topic title' + key, 'test topic content' + key, 'share', authorId);
 };
 
-const createReply = exports.createReply = async function (topicId, authorId) {
+exports.createReply = async function(topicId, authorId) {
   return await Reply.newAndSaveReply('I am content', topicId, authorId);
 };
 
-const createSingleUp = exports.createSingleUp = async function (replyId, userId) {
+exports.createSingleUp = async function(replyId, userId) {
   const reply = await Reply.getReply(replyId);
   reply.ups = [userId];
   await reply.save();
   return reply;
 };
 
-function mockUser(user) {
-  return 'mock_user=' + JSON.stringify(user) + ';';
-}
-
-exports.initTestData = async function () {
+(async function () {
   try {
     const [user, user2, admin] = await Promise.all([
-      createUser(),
-      createUser(),
-      createUser()
+      exports.createUser(),
+      exports.createUser(),
+      exports.createUser()
     ]);
 
     exports.normalUser = user;
@@ -65,16 +65,14 @@ exports.initTestData = async function () {
     exports.adminUser = admin;
     exports.adminUserCookie = mockUser(adminObj);
 
-    const topic = await createTopic(user._id);
+    const topic = await exports.createTopic(user._id);
     exports.testTopic = topic;
 
-    const reply = await createReply(topic._id, user._id);
+    const reply = await exports.createReply(topic._id, user._id);
     exports.testReply = reply;
 
   } catch (err) {
     console.error('Init test data error: ', err);
     throw err;
   }
-};
-
-exports.initTestData();
+})();
